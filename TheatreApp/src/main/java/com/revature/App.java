381bf6c5abc0c28ac2839a8134d15193aa84b2bb
ryan-mcomber <ryanmcom@hotmail.com>
2021-08-26 13:35:04 -0600

    notes updated 8/26

--- TheatreApp/src/main/java/com/revature/App.java
+++ TheatreApp/src/main/java/com/revature/App.java
@@ -2,9 +2,28 @@ package com.revature;
 
 public class App {
 
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Theatre theatre = new Theatre("Ford's Theatre", 8, 12);
+		theatre.getSeats();
 
+		// I want to reserve a seat.
+		
+		//I'll enter the seatNumber, and it must go through a method in the Theatre class to check if it's reserved or not.
+		if(theatre.reserveSeatBinary("H12")) { // O(log n) -- logarithmic
+			System.out.println("Please pay to reserve this free seat.");
+			
+		}else {
+			System.out.println("The seat is reserved,sorry.");
+		}
+		
 	}
 
 }
--- TheatreApp/src/main/java/com/revature/Theatre.java
+++ TheatreApp/src/main/java/com/revature/Theatre.java
@@ -8,6 +8,19 @@ public class Theatre {
 	private final String theatreName; // we will initialize it inside of the constructor
 	private List<Seat> seats = new ArrayList<>();
 	
+	/**
+	 * 
+	 * When do you an ArrayList over a LinkedList and vice versa
+	 * 
+	 * ArrayList:
+	 * best Searching: is done in O(1) constant time
+	 * Worst for Inserting (and deleting): done in O(n) linear time
+	 * 
+	 * LinkedList:
+	 * best for INSERTING/DELETING: O(1) constant time (because LinkedList consists of nodes that addresses)
+	 * Worst for: Searching sometime O(1) other times more complex...
+	 */
+	
 	public Theatre(String theatreName, int numRows, int seatsPerRow) {
 		
 		this.theatreName = theatreName;
@@ -31,7 +44,82 @@ public class Theatre {
 	}
 	
 	
-	private class Seat {
+	public String getTheatreName() {
+		return theatreName;
+	}
+	
+	public void getSeats() {
+		for (Seat seat: seats) {
+			System.out.println(seat.getSeatNumber());
+		}
+	}
+	
+	//I'd like to reserve seat G07
+	//BRUTE FORCE ALGORITHM - I will check every single seat to check if it's the one I want to reserve
+	public boolean reserveSeat(String seatNumber) { //someone could enter a non existent seat number
+		
+		Seat requestedSeat = null;
+		for (Seat seat: seats) {
+			
+			System.out.println(".");
+			
+			if(seat.getSeatNumber().equals(seatNumber)) {
+				requestedSeat =seat;
+				break;
+			}
+			
+		}
+		
+		if (requestedSeat == null) {
+			System.out.println("There is no seat "+seatNumber);
+			return false;
+		}
+		return requestedSeat.reserve();
+		
+	}
+	
+	// Binary Reserve Seat Algorithm
+	public boolean reserveSeatBinary(String seatNumber) {
+		
+		int low = 0;
+		int high = seats.size();
+		
+		while(low<high) {
+			
+			System.out.println(".");
+			
+			//select the middle value
+			int mid = (low+high)/2;
+			
+			// use the middle value to capture the middle seat of the row
+			Seat midVal = seats.get(mid); //get is a method
+			
+			//we have to check is the seatNumber pass through greater than or less than the midVal?
+			int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+			
+			//0 if equal, -1 if less than, 1 if greater than
+			if(cmp <0) {
+				low = mid+1;
+			} else if (cmp>0) {
+				high = mid -1;
+			} else {
+				return seats.get(mid).reserve();
+			}
+			
+		}
+		
+		System.out.println("There is no seat "+seatNumber);
+		return false;
+	}
+
+	//this is a nested class
+	/*
+	 * Comparable is a special interface that allows us to define the order of a user-defined class
+	 * 
+	 *  
+	 */
+	
+	private class Seat implements Comparable<Seat>{ //Comparable is an interface we use to define order within a user-created class
 		
 		private final String seatNumber; // A11 - G11
 		private boolean reserved = false;
@@ -43,6 +131,24 @@ public class Theatre {
 
 		public String getSeatNumber() {
 			return seatNumber;
+		}
+		
+		public boolean reserve() {
+			if(!this.reserved) {
+				this.reserved = true;
+				
+				System.out.println("Reservation of seat "+seatNumber+" confirmed.");
+				
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		//this is how we define if a seat is greater than or less than another
+		@Override
+		public int compareTo(Seat seat) {
+			return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
 		}
 		
 		
